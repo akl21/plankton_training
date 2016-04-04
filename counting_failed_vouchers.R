4204629c7bb8806edb8e3f09e917e2e88fd7e5ce
akl21 <akl21@duke.edu>
2016-04-04 11:03:41 -0400

    Add ggplot barplot

--- counting_failed_vouchers.R
+++ counting_failed_vouchers.R
@@ -4,32 +4,19 @@ phylum_table <- read.csv("data/phylum_listings.csv",
                          stringsAsFactors= FALSE)
 
 library("dplyr")
-library(ggplot2)
+library("ggplot2")
 
-#This function takes an input of a data frame, selects for the 
-#voucher number and pcr_success columns, filters out the voucher numbers
-#for which the PCR failed every time, then finally produces a count 
-#of how many times the PCR failed for each distinct voucher number.
-count_failed_vouchers <- function(pcr_data_frame){
-  select(pcr_data_frame, voucher_number, pcr_success) %>%
-    filter(pcr_success == 0 & !(pcr_success==1)) %>%
-    distinct(voucher_number) %>%
-    group_by(voucher_number) %>%
-    summarize(failed_vouchers = n())
-} 
-
-count_failed_vouchers(pcr_table)
-
-#This code block inputs our PCR table into the count_failed_vouchers
+#This code block inputs our PCR table into the global environment's summarize_pcr_failures
 #function, then left joins it with the table containing the phylums associated
 #with the voucher numbers, and outputs a table listing the phylum and number of
 #distinct samples that failed when attempting PCR. 
 #The resulting table is named pcr_failure_table.
 
-pcr_failure_table <- count_failed_vouchers(pcr_table) %>%
+pcr_failure_table <- summarize_pcr_failures(pcr_table) %>%
   left_join(phylum_table) %>%
   select(failed_vouchers, phylum) %>%
   group_by(phylum) %>%
+  distinct(voucher_number)
   summarize(failed_vouchers = n())
 pcr_failure_table
 
@@ -50,9 +37,10 @@ pcr_percent_failure <- num_samples_tested %>%
 
 pcr_percent_failure
 
-#The following code produces a simple barplot of fraction of PCR failure 
+#The following code produces a barplot in ggplot2 of fraction of PCR failure 
 #for each phylum.
-barplot(pcr_percent_failure[,2], main = "Fraction of PCR Failure", xlab = "Phyla", 
-        names.arg = pcr_percent_failure[,1])
-
+df_percent_failure = data.frame(pcr_percent_failure)
+ggplot(df_percent_failure, aes(pcr_percent_failure[,1],pcr_percent_failure[,2])) +
+  geom_bar(stat = "identity") + labs(x = "Phyla", y = "Sample Failure Fraction", 
+                                     title = "Fraction of PCR Failure")
 
