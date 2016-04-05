c35d3b342c945251d4fb5d41d958773de15fca4d
akl21 <akl21@duke.edu>
2016-04-05 08:32:12 -0400

    Make minor edits to clean up presentation of code.

--- counting_failed_vouchers.R
+++ counting_failed_vouchers.R
@@ -5,28 +5,27 @@ phylum_table <- read.csv("data/phylum_listings.csv",
 
 library("dplyr")
 library("ggplot2")
+summarize_pcr_failures(pcr_table)
 
-#This code block inputs our PCR table into the global environment's summarize_pcr_failures
-#function, then left joins it with the table containing the phylums associated
-#with the voucher numbers, and outputs a table listing the phylum and number of
-#distinct samples that failed when attempting PCR. 
+#This code block outputs a table listing the phylum and number of
+#distinct samples in that phylum that failed when attempting PCR. 
 #The resulting table is named pcr_failure_table.
-
 pcr_failure_table <- summarize_pcr_failures(pcr_table) %>%
   left_join(phylum_table) %>%
-  select(failed_vouchers, phylum) %>%
+  distinct(voucher_number) %>%
+  select(pcr_failure, phylum) %>%
   group_by(phylum) %>%
-  distinct(voucher_number)
-  summarize(failed_vouchers = n())
+  summarize(pcr_failure = n())
 pcr_failure_table
 
-#In this code block I try to count how many vouchers were tested for each phylum.
+#In this code block I try to count how many distinct vouchers were tested for each phylum.
 num_samples_tested <- pcr_table %>%
   left_join(phylum_table) %>%
   select(voucher_number, phylum) %>%
   distinct(voucher_number) %>%
   group_by(phylum) %>%
   summarize(num_vouchers = n())
+num_samples_tested
 
 #Here I present the percent of samples that fail during PCR by phylum out of the 
 #total number of distinct samples tested.
@@ -37,7 +36,7 @@ pcr_percent_failure <- num_samples_tested %>%
 
 pcr_percent_failure
 
-#The following code produces a barplot in ggplot2 of fraction of PCR failure 
+#The following code produces a barplot in ggplot2 of the fraction of PCR failure 
 #for each phylum.
 df_percent_failure = data.frame(pcr_percent_failure)
 ggplot(df_percent_failure, aes(pcr_percent_failure[,1],pcr_percent_failure[,2])) +
